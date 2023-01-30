c2b829c3e0a86443d69acd327f5c783f5f22210d
rmaniva <rmaniva@NLTI159>
2023-01-30 17:42:03 +0530

    Add project files.

---
+++ sample program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class EvenOddProgram
+{
+    public static void Main()
+    {
+        int num;
+        Console.Write("Enter a Number : ");
+        num = int.Parse(Console.ReadLine());
+
+        if (num % 2 == 0)
+        {
+            Console.WriteLine(num + " is an Even Number");
+        }
+        else
+        {
+            Console.WriteLine(num + " is an Odd Number");
+        }
+    }
+}
